813f07209066ce9f3ca8575977a8c64ccec9c05c
Jonsson <yz808@outlook.com>
2020-07-15 21:05:47 +0800

    改名

--- src/main/java/com/card/FakaApplication.java
+++ src/main/java/com/card/FakaApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class DangmianfuApplication {
+public class FakaApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(DangmianfuApplication.class, args);
+        SpringApplication.run(FakaApplication.class, args);
     }
 
 }
