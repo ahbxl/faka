53d2676f48c9108f31c7b81d235dc74c57128590
Jonsson <yz808@outlook.com>
2020-07-22 00:20:15 +0800

    提交

---
+++ src/main/java/com/card/config/ShiroConfig.java
@@ -0,0 +1,30 @@
+package com.card.config;
+
+import org.apache.shiro.mgt.DefaultSecurityManager;
+import org.apache.shiro.spring.web.ShiroFilterFactoryBean;
+import org.apache.shiro.web.mgt.DefaultWebSecurityManager;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class ShiroConfig {
+    @Bean
+    public ShiroFilterFactoryBean getShiroFilterFactoryBean(@Qualifier("getDefaultSecurityManager") DefaultSecurityManager defaultSecurityManager) {
+        ShiroFilterFactoryBean shiroFilterFactoryBean = new ShiroFilterFactoryBean();
+        shiroFilterFactoryBean.setSecurityManager(defaultSecurityManager);
+        return shiroFilterFactoryBean;
+    }
+
+    @Bean
+    public DefaultWebSecurityManager getDefaultSecurityManager(@Qualifier("getUserRealm") UserRealm userRealm) {
+        DefaultWebSecurityManager defaultWebSecurityManager = new DefaultWebSecurityManager();
+        defaultWebSecurityManager.setRealm(userRealm);
+        return defaultWebSecurityManager;
+    }
+
+    @Bean
+    public UserRealm getUserRealm() {
+        return new UserRealm();
+    }
+}
--- src/main/java/com/card/controller/AdminController.java
+++ src/main/java/com/card/controller/AdminController.java
@@ -1,11 +1,15 @@
 package com.card.controller;
 
 import com.card.command.admin.LoginCommand;
-import com.card.entity.domain.Admin;
 import com.card.entity.vo.ResultVO;
 import com.card.service.AdminService;
 import com.card.util.ResultVOUtil;
 import lombok.extern.slf4j.Slf4j;
+import org.apache.shiro.SecurityUtils;
+import org.apache.shiro.authc.IncorrectCredentialsException;
+import org.apache.shiro.authc.UnknownAccountException;
+import org.apache.shiro.authc.UsernamePasswordToken;
+import org.apache.shiro.subject.Subject;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
@@ -20,11 +24,16 @@ public class AdminController {
     @PostMapping("/login")
     @ResponseBody
     public ResultVO<Object> findByUsernameAndPassword(@RequestBody LoginCommand command) {
-        Admin admin = adminService.findByUsernameAndPassword(command.getUsername(), command.getPassword());
-        if (admin != null) {
-            return ResultVOUtil.success(admin);
+        Subject subject = SecurityUtils.getSubject();
+        UsernamePasswordToken usernamePasswordToken = new UsernamePasswordToken(command.getUsername(), command.getPassword());
+        try {
+            subject.login(usernamePasswordToken);
+        } catch (UnknownAccountException e) {
+            return ResultVOUtil.success("用户名不存在");
+        } catch (IncorrectCredentialsException e) {
+            return ResultVOUtil.success("密码错误");
         }
-        return ResultVOUtil.success("用户名或者密码输入错误！");
+        return ResultVOUtil.success("登录失败");
     }
 
     @PostMapping("/countUsername/{username}")
