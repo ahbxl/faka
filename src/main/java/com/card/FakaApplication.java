87cbdb38c7055b0ccdd347140dcb9b0ea30e29fd
JonssonYan <yz808@outlook.com>
2020-08-18 23:47:19 +0800

    系统启动时创建项目文件夹

--- src/main/java/com/card/FakaApplication.java
+++ src/main/java/com/card/FakaApplication.java
@@ -1,12 +1,23 @@
 package com.card;
 
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import java.io.File;
+
 @SpringBootApplication
-public class FakaApplication {
+public class FakaApplication implements CommandLineRunner {
 
     public static void main(String[] args) {
         SpringApplication.run(FakaApplication.class, args);
+    }
+
+    @Override
+    public void run(String... args) {
+        File file = new File("/data/faka/exportFile/");
+        if (!file.exists()) {
+            file.mkdir();
+        }
     }
 }
