e67a375c9ad39127c2ec8b579bdb1fcbdfd12ddb
jonssonyan <yz808@outlook.com>
2021-01-12 00:46:37 +0800

    feat:sql

--- src/main/resources/sql/_localhost-2021_01_12_00_46_13-dump.sql
+++ src/main/resources/sql/_localhost-2021_01_12_00_46_13-dump.sql
@@ -25,7 +25,7 @@ DROP TABLE IF EXISTS `ali_pay_config`;
 CREATE TABLE `ali_pay_config` (
   `id` bigint(20) NOT NULL AUTO_INCREMENT COMMENT '主键',
   `user_id` bigint(20) DEFAULT NULL COMMENT '用户id',
-  `app_id` varchar(255) DEFAULT NULL COMMENT 'appid',
+  `app_id` varchar(50) DEFAULT NULL COMMENT 'appid',
   `merchant_private_key` text COMMENT '私钥',
   `ali_pay_public_key` text COMMENT '支付宝公钥',
   `state` tinyint(4) NOT NULL DEFAULT '1' COMMENT '状态 1/正常',
@@ -47,7 +47,7 @@ CREATE TABLE `card` (
   `user_id` bigint(20) DEFAULT NULL COMMENT '购买人的用户id',
   `product_id` bigint(20) DEFAULT NULL COMMENT '商品id',
   `creator` bigint(20) DEFAULT NULL COMMENT '创建人',
-  `content` varchar(255) DEFAULT NULL COMMENT '卡密信息',
+  `content` varchar(100) DEFAULT NULL COMMENT '卡密信息',
   `state` tinyint(1) NOT NULL DEFAULT '0' COMMENT '状态：0/未售出 1/已售出',
   `create_time` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP COMMENT '创建时间',
   `update_time` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP COMMENT '更新时间',
@@ -64,7 +64,7 @@ DROP TABLE IF EXISTS `category`;
 /*!40101 SET character_set_client = utf8 */;
 CREATE TABLE `category` (
   `id` bigint(20) NOT NULL AUTO_INCREMENT COMMENT '主键',
-  `name` varchar(255) DEFAULT NULL COMMENT '名称',
+  `name` varchar(20) DEFAULT NULL COMMENT '名称',
   `parent_id` bigint(20) DEFAULT NULL COMMENT '父级id',
   `creator` bigint(20) DEFAULT NULL COMMENT '创建人',
   `state` tinyint(4) NOT NULL DEFAULT '1' COMMENT '状态：0/下架 1/上架',
@@ -83,8 +83,8 @@ DROP TABLE IF EXISTS `export_file`;
 /*!40101 SET character_set_client = utf8 */;
 CREATE TABLE `export_file` (
   `id` bigint(20) NOT NULL AUTO_INCREMENT COMMENT '主键',
-  `name` varchar(255) DEFAULT NULL COMMENT '文件名称',
-  `path` varchar(255) DEFAULT NULL COMMENT '文件路径',
+  `name` varchar(20) DEFAULT NULL COMMENT '文件名称',
+  `path` varchar(50) DEFAULT NULL COMMENT '文件路径',
   `creator` bigint(20) DEFAULT NULL COMMENT '创建人',
   `state` tinyint(4) NOT NULL DEFAULT '-1' COMMENT '状态 0/未下载 1/已下载 -1/正在生成',
   `create_time` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP COMMENT '创建时间',
@@ -103,9 +103,10 @@ DROP TABLE IF EXISTS `menu_list`;
 CREATE TABLE `menu_list` (
   `id` bigint(20) NOT NULL AUTO_INCREMENT COMMENT '主键',
   `parent_id` bigint(20) DEFAULT NULL COMMENT '父级id',
-  `name` varchar(255) DEFAULT NULL COMMENT '名称',
+  `name` varchar(20) DEFAULT NULL COMMENT '名称',
+  `icon` varchar(20) DEFAULT NULL COMMENT '菜单图标',
   `priority` int(11) DEFAULT NULL COMMENT '优先级',
-  `path` varchar(255) DEFAULT NULL COMMENT '路由',
+  `path` varchar(50) DEFAULT NULL COMMENT '路由',
   `state` tinyint(4) NOT NULL DEFAULT '1' COMMENT '状态 1/正常',
   `create_time` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP COMMENT '创建时间',
   `update_time` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP COMMENT '更新时间',
@@ -125,9 +126,9 @@ CREATE TABLE `order` (
   `product_id` bigint(20) DEFAULT NULL COMMENT '商品id',
   `creator` bigint(20) DEFAULT NULL COMMENT '创建人',
   `quantity` bigint(20) DEFAULT NULL COMMENT '数量',
-  `subject` varchar(255) DEFAULT NULL COMMENT '订单标题',
-  `outTradeNo` varchar(255) DEFAULT NULL COMMENT '商户订单号',
-  `total_amount` varchar(255) DEFAULT NULL COMMENT '总金额',
+  `subject` varchar(20) DEFAULT NULL COMMENT '订单标题',
+  `outTradeNo` varchar(50) DEFAULT NULL COMMENT '商户订单号',
+  `total_amount` decimal(10,2) DEFAULT NULL COMMENT '总金额',
   `state` tinyint(4) NOT NULL DEFAULT '0' COMMENT '状态 0：未支付 1：已支付',
   `create_time` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP COMMENT '创建时间',
   `update_time` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP COMMENT '更新时间',
@@ -144,8 +145,8 @@ DROP TABLE IF EXISTS `permission`;
 /*!40101 SET character_set_client = utf8 */;
 CREATE TABLE `permission` (
   `id` bigint(20) NOT NULL AUTO_INCREMENT COMMENT '主键',
-  `name` varchar(255) DEFAULT NULL COMMENT '权限标识',
-  `description` varchar(255) DEFAULT NULL COMMENT '权限描述',
+  `name` varchar(100) DEFAULT NULL COMMENT '权限标识',
+  `description` varchar(50) DEFAULT NULL COMMENT '权限描述',
   `state` tinyint(4) NOT NULL DEFAULT '1' COMMENT '状态 1/正常',
   `create_time` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP COMMENT '创建时间',
   `update_time` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP COMMENT '更新时间',
@@ -163,8 +164,8 @@ DROP TABLE IF EXISTS `product`;
 CREATE TABLE `product` (
   `id` bigint(20) NOT NULL AUTO_INCREMENT COMMENT '主键',
   `category_id` bigint(20) DEFAULT NULL COMMENT '分类id',
-  `name` varchar(255) DEFAULT NULL COMMENT '名称',
-  `price` decimal(10,0) DEFAULT NULL COMMENT '价格',
+  `name` varchar(20) DEFAULT NULL COMMENT '名称',
+  `price` decimal(10,2) DEFAULT NULL COMMENT '价格',
   `creator` bigint(20) DEFAULT NULL COMMENT '创建人',
   `state` tinyint(4) NOT NULL DEFAULT '1' COMMENT '状态：0/下架 1/上架',
   `create_time` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP COMMENT '创建时间',
@@ -183,7 +184,7 @@ DROP TABLE IF EXISTS `role`;
 CREATE TABLE `role` (
   `id` bigint(20) NOT NULL AUTO_INCREMENT COMMENT '主键',
   `parent_id` bigint(20) DEFAULT NULL COMMENT '上级角色',
-  `name` varchar(255) DEFAULT NULL COMMENT '角色名称',
+  `name` varchar(20) DEFAULT NULL COMMENT '角色名称',
   `state` tinyint(4) NOT NULL DEFAULT '1' COMMENT '状态 1/正常',
   `create_time` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP COMMENT '创建时间',
   `update_time` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP COMMENT '更新时间',
@@ -239,8 +240,8 @@ CREATE TABLE `user` (
   `parent_id` bigint(20) DEFAULT NULL COMMENT '上级id',
   `domain` varchar(20) DEFAULT NULL COMMENT '域名',
   `role_id` bigint(20) DEFAULT NULL COMMENT '角色id',
-  `username` varchar(255) DEFAULT NULL COMMENT '用户名',
-  `password` varchar(255) DEFAULT NULL COMMENT '密码',
+  `username` varchar(20) DEFAULT NULL COMMENT '用户名',
+  `password` varchar(50) DEFAULT NULL COMMENT '密码',
   `email` varchar(20) DEFAULT NULL COMMENT '邮箱',
   `qq` varchar(20) DEFAULT NULL COMMENT 'qq号',
   `phone` varchar(20) DEFAULT NULL COMMENT '联系方式',
@@ -248,7 +249,7 @@ CREATE TABLE `user` (
   `create_time` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP COMMENT '创建时间',
   `update_time` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP COMMENT '更新时间',
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8;
+) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=utf8;
 /*!40101 SET character_set_client = @saved_cs_client */;
 /*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
 
@@ -260,4 +261,4 @@ CREATE TABLE `user` (
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2021-01-10 23:07:55
+-- Dump completed on 2021-01-12  0:46:13
